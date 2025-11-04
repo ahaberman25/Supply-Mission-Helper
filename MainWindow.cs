7629204af0b1cc45fbc62cfe04f561fc64b1f6ba
ahaberman25 <a.haberman25@gmail.com>
2025-11-04 00:10:38 -0500

    fix

--- MainWindow.cs
+++ MainWindow.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Numerics;
 using Dalamud.Interface.Windowing;
-using ImGuiNET;
 
 namespace SupplyMissionHelper;
 
@@ -10,11 +9,11 @@ public class MainWindow : Window, IDisposable
     private Plugin Plugin { get; init; }
 
     public MainWindow(Plugin plugin)
-        : base("Supply Mission Helper", ImGuiWindowFlags.None)
+        : base("Supply Mission Helper")
     {
         SizeConstraints = new WindowSizeConstraints
         {
-            MinimumSize = new Vector2(500, 400),
+            MinimumSize = new Vector2(400, 300),
             MaximumSize = new Vector2(float.MaxValue, float.MaxValue)
         };
 
@@ -25,45 +24,6 @@ public class MainWindow : Window, IDisposable
 
     public override void Draw()
     {
-        ImGui.Text("Supply Mission Helper");
-        ImGui.Spacing();
-
-        ImGui.TextWrapped("Welcome to Supply Mission Helper!");
-        ImGui.Spacing();
-
-        ImGui.Separator();
-        ImGui.Spacing();
-
-        ImGui.Text("Instructions:");
-        ImGui.BulletText("Open your Grand Company Supply Mission window");
-        ImGui.BulletText("Click 'Scan Supply Missions' button (coming soon)");
-        ImGui.BulletText("View the materials needed to complete your missions");
-
-        ImGui.Spacing();
-        ImGui.Separator();
-        ImGui.Spacing();
-
-        ImGui.Text("Configuration:");
-        
-        var showRaw = Plugin.Configuration.ShowRawMaterialsOnly;
-        if (ImGui.Checkbox("Show Raw Materials Only", ref showRaw))
-        {
-            Plugin.Configuration.ShowRawMaterialsOnly = showRaw;
-            Plugin.Configuration.Save();
-        }
-
-        var includeLocations = Plugin.Configuration.IncludeGatheringLocations;
-        if (ImGui.Checkbox("Include Gathering Locations", ref includeLocations))
-        {
-            Plugin.Configuration.IncludeGatheringLocations = includeLocations;
-            Plugin.Configuration.Save();
-        }
-
-        ImGui.Spacing();
-        ImGui.Separator();
-        ImGui.Spacing();
-
-        ImGui.TextColored(new Vector4(1.0f, 1.0f, 0.0f, 1.0f), "Status: Plugin loaded successfully!");
-        ImGui.TextWrapped("Scanning functionality will be added in a future update.");
+        // Placeholder - UI will be added once we confirm ImGui availability
     }
 }
